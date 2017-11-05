e71b0ac3016e7cc3411190e32fdb3d2c15bce282
24967 <forestlii@outlook.com>
2017-11-05 18:42:50 +0800

    CREATEPROJECTS

---
+++ Assets/Scripts/TestTen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTen : MonoBehaviour {
+
+	private string webName;
+	private int num;
+	void Start () {
+		Debug.Log(webName);
+		Debug.Log(num);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
